78a724c6b072a7bf3c458efe79fb3797f1df092b
kmm337 <kmorrissey@u.northwestern.edu>
2021-07-12 23:59:28 -0500

    line maps

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,61 +2,56 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7,-94.5], 4);
+let map = L.map('mapid').setView([37.6213,-122.3790], 5);
 
-// An array containing each city's location, state, and population.
-// Get data from cities.js
-let cityData = cities;
-  
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//   console.log(city)
-//   L.marker(city.location)
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-// .addTo(map);
-// });
+// Coordinates for each point to be used in the polyline.
+// LAX, SFO, SLC, and SEA
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
 
-// bind a pop-up to the marker showing city name and population
-// cityData.forEach(function(city) {
-//   console.log(city)
-//   L.circleMarker(city.location, {
-//     radius: city.population/100000
-//   })
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: 'orange',
-    fillColor: 'orange',
-    weight: 4
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
-
-// bind a pop-up to the marker showing city name and population
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location).addTo(map).bindPopup(
-//       [city.city,
-//       city.population.join("<br>")]
-//     );
-// });
-   
-
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18, 
     tileSize: 512,  
     zoomOffset: -1,
     accessToken: API_KEY
 });
+
+// New airline route: SFO, AUS, YYZ, JFK
+
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// let line = [
+//   [37.6213, -122.3790],
+//   [30.18999924, -97.668663992],
+//   [43.678524, -79.629129],
+//   [40.641766, -73.780968]
+// ];
+// L.polyline(line, {
+//   stroke: true,
+//   dashArray: "5, 10",
+//   dashOffset: "0",
+//   color: "blue",
+//   weight: 4,
+//   opacity: .5
+// }).addTo(map);
+
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18, 
+//     tileSize: 512,  
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
