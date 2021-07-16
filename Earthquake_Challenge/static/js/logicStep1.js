f233b4e52d0ba9890288a71f11f396e73e77e987
kmm337 <kmorrissey@u.northwestern.edu>
2021-07-16 16:27:25 -0500

    challenge draft

---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,48 @@
+// Step 1 map all recorded earthquakes from the past seven days
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// create the street view tile layer that will be an option for the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18, 
+    accessToken: API_KEY
+});
+// create the satellite street view tile layer that will be an option for the map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18, 
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+// Create the map object with center of the US, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  //Creating a GeoJSON layer with retrieved data
+  L.geoJson(data).addTo(map);
+});
+    
+//     , {
+//   style: myStyle,
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+//   }
+// })
+// .addTo(map);
+// });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mapping_Earthquakes
+
+## Purpose:
+
+Provide maps that show earthquake data in relation to the tectonic plates’ location on the earth. Provide the ability to display just the major earthquakes (magnitude > 4.5).
+
+Deliverable 1: Add Tectonic Plate Data
+Deliverable 2: Add Major Earthquake Data
+Deliverable 3: Add an Additional Map
